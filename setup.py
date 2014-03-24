503c047b2f8a053017fef03a3c99c07f81290392
Jonathan Tushman <jtushman@pipewave.com>
2014-03-24 15:01:01 -0400

    working on packing and travis

--- setup.py
+++ setup.py
@@ -1,17 +1,19 @@
 from setuptools import setup
 
-required_modules = ['bouncer']
+required_modules = ['bouncer', 'Flask>=0.9']
 
-setup(name='flask_bouncer',
+setup(name='flask-bouncer',
       version='0.0.1',
-      description='Simple Declarative Authentication based on Ryan Bates excellent cancan library',
-      url='http://github.com/jtushman/bouncer',
+      description='Flask Simple Declarative Authentication based on Ryan Bates excellent cancan library',
+      url='http://github.com/jtushman/flask-bouncer',
       author='Jonathan Tushman',
       author_email='jonathan@zefr.com',
       install_requires=required_modules,
       license='MIT',
-      packages=['flask_abilities'],
+      py_mocdules=['flask_bouncer'],
       zip_safe=False,
+      platforms='any',
+      test_suite='nose.collector',
       classifiers=[
           'Environment :: Web Environment',
           'Intended Audience :: Developers',
