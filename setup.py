e137e90c62c4d59f240ae44b7ea28b34d78fc323
Jonathan Tushman <jtushman@pipewave.com>
2014-03-26 11:05:46 -0400

    setup bug

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup
 required_modules = ['bouncer>=0.1', 'Flask>=0.9', 'blinker']
 
 setup(name='flask-bouncer',
-      version='0.1.1',
+      version='0.1.2',
       description='Flask Simple Declarative Authentication based on Ryan Bates excellent cancan library',
       url='http://github.com/jtushman/flask-bouncer',
       author='Jonathan Tushman',
       author_email='jonathan@zefr.com',
       install_requires=required_modules,
       license='MIT',
-      py_mocdules=['flask_bouncer'],
+      py_modules=['flask_bouncer'],
       zip_safe=False,
       platforms='any',
       tests_require=['nose'],
