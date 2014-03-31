ec19e4fadac19de3a3263b0c02f07715e14057af
Jonathan Tushman <jtushman@pipewave.com>
2014-03-31 09:28:53 -0400

    removing test

--- test_flask_bouncer/test_advanced_usage.py
+++ test_flask_bouncer/test_advanced_usage.py
@@ -11,37 +11,6 @@ def test_non_standard_names():
     app.debug = True
     bouncer = Bouncer(app)
 
-    bouncer.alias_actions = {
-        'browse': 'read'
-    }
-
-    @bouncer.authorization_method
-    def define_authorization(user, they):
-        they.can('browse', Article)
-
-    @app.route("/articles")
-    @requires('browse', Article)
-    def articles_index():
-        return "A bunch of articles"
-
-    client = app.test_client()
-
-    jonathan = User(name='jonathan', admin=False)
-    with user_set(app, jonathan):
-        resp = client.get('/articles')
-        eq_(b"A bunch of articles", resp.data)
-
-
-def test_action_aliases():
-
-    app = Flask("aliases")
-    app.debug = True
-    bouncer = Bouncer(app)
-
-    # bouncer.alias_actions = {
-    #     'read': 'browse'
-    # }
-
     @bouncer.authorization_method
     def define_authorization(user, they):
         they.can('browse', Article)
