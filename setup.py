36b2e4b5495b9807b48feb21206334ec18a5132c
Jonathan Tushman <jtushman@pipewave.com>
2014-04-01 11:35:25 -0400

    setup.py tweak

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(name='flask-bouncer',
       py_modules=['flask_bouncer'],
       zip_safe=False,
       platforms='any',
-      tests_require=['nose','flask-classy>=0.6.9'],
+      tests_require=['nose','flask-classy'],
       test_suite='nose.collector',
       classifiers=[
           'Environment :: Web Environment',
