e08488d2ede896510c953f12f1364c658aec433c
Jonathan Tushman <jtushman@pipewave.com>
2014-04-05 14:51:50 -0400

    doc cleanup

--- flask_bouncer.py
+++ flask_bouncer.py
@@ -11,7 +11,7 @@ def ensure(action, subject):
     ability.authorization_method = current_app.bouncer.get_authorization_method()
     ability.aliased_actions = current_app.bouncer.alias_actions
     if ability.cannot(action, subject):
-        msg = "{} does not have {} access to {}".format(current_user, action, subject)
+        msg = "{0} does not have {1} access to {2}".format(current_user, action, subject)
         raise Unauthorized(msg)
 
 #alais
