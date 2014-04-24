6ec5964f989cc1edd16dea2452f2d92341ab05c7
Jonathan Tushman <jtushman@pipewave.com>
2014-04-23 20:48:27 -0400

    bump + adding AUTHORS and CHANGES

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 required_modules = ['bouncer>=0.1.8', 'Flask>=0.9', 'blinker']
 
 setup(name='flask-bouncer',
-      version='0.1.12',
+      version='0.1.13',
       description='Flask Simple Declarative Authentication based on Ryan Bates excellent cancan library',
       url='http://github.com/jtushman/flask-bouncer',
       author='Jonathan Tushman',
